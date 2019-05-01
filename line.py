4703bda85c32534b23c54d98b8f4d4db83f299bf
Benedek László <41867355+DowerX@users.noreply.github.com>
2019-05-01 08:39:17 +0200

    Minor opt

--- line.py
+++ line.py
@@ -1,19 +1,19 @@
 import pygame as pygame
 import pyaudio
 import struct
-import time
 
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 #SETTINGS
-size = (1024, 256)
+size = (256, 256)
 chunk = 1024
 stepsize = 1
 filter_255 = False
 filter_2 = True
-max_fps = 30
+
+max_fps = 100
 width_of_col = 1
 scale = 1
 skip_under = 0
@@ -25,46 +25,29 @@ pygame.display.set_caption("AV")
 done = False
 clock = pygame.time.Clock()
 
+class AudioStream():
+    def __init__(self,device_index,device_channels=1,inp=True,outp=True,chunksize=1024):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format=pyaudio.paInt16,
+            channels=device_channels,
+            rate=44100,
+            input=inp,
+            output=outp,
+            #as_loopback=True,
+            input_device_index=device_index, #0 laptop ,1 loopback , 3 fejes
+            frames_per_buffer=chunksize
+        )
 
-p = pyaudio.PyAudio()
-stream = p.open(
-    format=pyaudio.paInt16,
-    channels=1,
-    rate=44100,
-    input=True,
-    output=True,
-    as_loopback=True,
-    input_device_index=1, #0 laptop ,1 loopback , 3 fejes
-    frames_per_buffer=chunk
-)
+recordStream = AudioStream(1,1,True,True,chunk)
+playbackStream = AudioStream(12,1,False,True,chunk)
 
-pp = pyaudio.PyAudio()
-out = pp.open(
-    format=pyaudio.paInt16,
-    channels=1,
-    rate=44100,
-    #input=True,
-    output=True,
-    input_device_index=12, #0 laptop ,1 loopback , 3 fejes
-    frames_per_buffer=chunk
-    )
-
+#USE THIS TO GET THE DEVICE INDEXES
 #for i in range(p.get_device_count()):
 #    print(f'{i} {p.get_device_info_by_index(i)["name"]}')
 
-
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-
-    temp = stream.read(chunk)
-    out.write(temp)
-    data = struct.unpack(f"{str(2*chunk)}B", temp)
-
-    screen.fill(BLACK)
-
+def draw(data):
+    global size, stepsize
     pygame.draw.line(screen, WHITE, (0, size[1]//2), (size[0], size[1]//2), 1)
 
     x = 0
@@ -83,15 +66,42 @@ while not done:
             continue
 
         if data[i*stepsize] > 127:
-            pygame.draw.line(screen, WHITE, (x, size[1]//2), (x, size[1]-data[i*stepsize]*scale+127*scale), width_of_col)
+            pygame.draw.line(screen, WHITE, (x, size[1]//2), (x, size[1]-data[i*stepsize]*scale+127), width_of_col)
         else:
             pygame.draw.line(screen, WHITE, (x, size[1]//2), (x, -data[i*stepsize]*scale+127), width_of_col)
         x += 1
 
-    pygame.display.flip()
 
-    #print(x)
-    pygame.display.set_caption(f"AV {int(clock.get_fps())+1}fps")
-    clock.tick(max_fps)
+def main():
+    global done, max_fps, chunk, screen, scale, clock
+    while not done:
+        # --- Main event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
 
-pygame.quit()
+        #read data
+        temp = recordStream.stream.read(chunk)
+        playbackStream.stream.write(temp)
+        data = struct.unpack(f"{str(2*chunk)}B", temp)
+
+        #clear screen
+        screen.fill(BLACK)
+
+        #draw new screen
+        draw(data)
+
+        #display new screen
+        pygame.display.flip()
+
+        #display fps
+        pygame.display.set_caption(f"Audio-visualisation {int(clock.get_fps())+1}fps")
+
+        #tick the clock
+        clock.tick(max_fps)
+
+    pygame.quit()
+    return
+
+if __name__ == "__main__":
+    main()
